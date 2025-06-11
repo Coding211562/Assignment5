ae9d65701a8fe1a8a0b7a709463638377f7a05a7
Nandini Badgujar <nandinibadgujar51203@gmail.com>
2025-06-11 14:00:12 +0530

    Add files via upload

---
+++ Assignment5_Task2.py
@@ -0,0 +1,10 @@
+#Task 2
+
+l1 = list(range(1,11))
+print("Original list :",l1)
+
+l2 = l1[0:5]
+print("Extracted first five elements :",l2)
+
+l3 = l2[::-1]
+print("Reversed extracted element :",l3)
